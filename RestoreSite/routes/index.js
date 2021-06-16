eee0e0cb7c89fcf7ea8644cecc1c0ea1a65d346f
JasonAsselin <45043553+JasonAsselin@users.noreply.github.com>
2021-06-16 16:33:40 -0400

    - set up new wood working project - set up new pug files - adding to stylesheets

--- RestoreSite/routes/index.js
+++ RestoreSite/routes/index.js
@@ -4,7 +4,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Wooden Restoration Inc.' });
+});
+
+/* GET projects and gallery page. */
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Projects and Gallery' });
+});
+
+/* GET about us page. */
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About Us' });
+});
+
+/* GET contact us page. */
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact Us' });
+});
+
+/* GET request for restore form page. */
+router.get('/rrf', function (req, res) {
+    res.render('rrf', { title: 'Request for Restoration Form' });
 });
 
 module.exports = router;
